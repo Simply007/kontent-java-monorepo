7caf706871e77babd9dcb8d975936f1666fc2fbb
ondrejch <ondrejch@kentico.com>
2020-06-24 09:03:54 +0200

    load content from Kontent

--- sample-app-spring-boot/src/main/java/kentico/kontent/delivery/sample/springboot/DemoApplication.java
+++ sample-app-spring-boot/src/main/java/kentico/kontent/delivery/sample/springboot/DemoApplication.java
@@ -1,10 +1,14 @@
 package kentico.kontent.delivery.sample.springboot;
 
+import kentico.kontent.delivery.ContentItemResponse;
+import kentico.kontent.delivery.DeliveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +18,12 @@ public class DemoApplication {
     }
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) throws ExecutionException, InterruptedException {
+        final DeliveryClient client = new DeliveryClient("975bf280-fd91-488c-994c-2f04416e5ee3");
+
+        ContentItemResponse aboutUs = client.getItem("about_us")
+                .toCompletableFuture()
+                .get();
+        return String.format("Hello %s!", aboutUs.getItem().getString("metadata__twitter_title"));
     }
 }
