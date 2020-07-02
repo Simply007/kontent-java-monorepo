af56db90ceae3b380c250fe9e0b486975e61f109
ondrejch <ondrejch@kentico.com>
2020-07-02 13:20:32 +0200

    remove comments

---
+++ kontent-delivery-generators-gradle/bin/main/kentico/kontent/delivery/generators/gradle/GenerateModelsExtension.groovy
@@ -0,0 +1,10 @@
+package kentico.kontent.delivery.generators.gradle
+
+import kentico.kontent.delivery.DeliveryClient
+
+class GenerateModelsExtension {
+    String projectId
+    String packageName
+    File outputDir
+    DeliveryClient deliveryClient
+}
