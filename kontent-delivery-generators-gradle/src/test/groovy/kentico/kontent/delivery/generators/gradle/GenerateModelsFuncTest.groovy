6cb1f370b90ea98362649755339d10637b22c39f
ondrejch <ondrejch@kentico.com>
2020-07-02 09:17:36 +0200

    fix tests locally

--- kontent-delivery-generators-gradle/src/test/groovy/kentico/kontent/delivery/generators/gradle/GenerateModelsFuncTest.groovy
+++ kontent-delivery-generators-gradle/src/test/groovy/kentico/kontent/delivery/generators/gradle/GenerateModelsFuncTest.groovy
@@ -133,7 +133,7 @@ class GenerateModelsFuncTest extends Specification {
     }
 
     class LocalServer extends LocalServerTestBase {
-        ServerBootstrap getProtectedServerBootstrap() {generateSources
+        ServerBootstrap getProtectedServerBootstrap() {
             return this.serverBootstrap
         }
     }
