0623500b120960117bf87a690a978d38ead441b5
ondrejch <ondrejch@kentico.com>
2020-07-07 16:31:18 +0200

    showcase the inline resolution for tweet (and a bug)

--- kontent-delivery/src/main/java/kentico/kontent/delivery/template/ViewResolverConfiguration.java
+++ kontent-delivery/src/main/java/kentico/kontent/delivery/template/ViewResolverConfiguration.java
@@ -34,7 +34,7 @@ public class ViewResolverConfiguration {
     String suffix = ".html";
 
     public ViewResolverConfiguration() {
-        this.prefixes.addAll(Arrays.asList("kentico/templates/", "META-INF/kentico/templates/"));
+        this.prefixes.addAll(Arrays.asList("kentico/templates/", "META-INF/kentico/templates/", "kontent/templates/", "META-INF/kontent/templates/"));
     }
 
     public ViewResolverConfiguration addPrefixes(String... prefixes) {
--- sample-app-spring-boot/src/main/java/kentico/kontent/delivery/sample/dancinggoat/springboot/KontentConfiguration.java
+++ sample-app-spring-boot/src/main/java/kentico/kontent/delivery/sample/dancinggoat/springboot/KontentConfiguration.java
@@ -1,6 +1,8 @@
 package kentico.kontent.delivery.sample.dancinggoat.springboot;
 
 import kentico.kontent.delivery.DeliveryClient;
+import kentico.kontent.delivery.InlineContentItemsResolver;
+import kentico.kontent.delivery.sample.dancinggoat.models.Tweet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,16 @@ public class KontentConfiguration {
     public DeliveryClient deliveryClient() {
         DeliveryClient client = new DeliveryClient("975bf280-fd91-488c-994c-2f04416e5ee3");
         client.scanClasspathForMappings("kentico.kontent.delivery.sample.dancinggoat.models");
+        // TODO - following line is necessary because in
+        //  ((java.util.HashMap.Node)client.stronglyTypedContentItemConverter.classToContentTypeMapping.entrySet().toArray()[0]).getKey()
+        //  there is the key duplicated for tweet - dunno why
+        client.registerType(Tweet.class);
+        client.registerInlineContentItemsResolver(new InlineContentItemsResolver<Tweet>() {
+            @Override
+            public String resolve(Tweet data) {
+                return "Inline resolution used for tweet!";
+            }
+        });
         return client;
     }
 }
